9cfb0aa869e3fa2fb740032e72a0a4290bdfbeff
DixitAnjali01 <122504032+DixitAnjali01@users.noreply.github.com>
2023-01-13 04:43:25 +0530

    Add files via upload

---
+++ p12.c
@@ -0,0 +1,15 @@
+//PROGRAM TO FIND LENGTH OF STRING USING POINTERS
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100],*p,count=0;
+    gets(str);
+    int i=0;
+    for(p=str;str[i];p++,i++)
+    {
+        count++;
+    }
+    printf("The length of string is =%d",count);
+ return 0;
+}
